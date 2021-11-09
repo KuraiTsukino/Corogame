cea775a5f48915a3f97f360e7faaf2bc782086e6
KuraiTsukino <sandym3000@hotmail.com>
2021-11-09 12:32:42 -0600

    archivo corregido

--- Corogame/js/main.js
+++ Corogame/js/main.js
@@ -13,85 +13,223 @@ const ctx = $canvas.getContext("2d");
 // Variables globales.
 
 let frames = 0;
-const gravity = 0.98;
-const friction = 0.9;
+const GRAVITY = 0.98;
+const friction = 0.7;
 let intervalId;
 
 // Clases del juego con propiedades y métodos.
 
-// Clase genérica.
-class GameAsset {
-    constructor(x, y, width, height, img) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+//prueba
+class Board {
+    constructor() {
+        this.x = 0; 
+        this.y = 0;
+        this.width = $canvas.width;
+        this.height = $canvas.height;
         this.image = new Image();
-        this.image.src = img;
-    }
-
-    draw() {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    }
-}
-
-class Board extends GameAsset {
-    constructor(x, y, width, height, img) {
-        super(x, y, width, height, img);
-        
+        this.image.src = "./../images/background.png"
     }
 
     draw () {
         this.x--;
+        // Efecto de fondo infinito.
         if (this.x < -this.width) this.x = 0;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         ctx.drawImage (this.image, this.x + this.width, this.y, this.width, this.height);
     } 
 }
 
-// Instancias. 
+class Character {
+    constructor(x, y) {
+        this.x = x; 
+        this.y = y;
+        this.width = 170;
+        this.height = 170;
+        this.image = new Image ();
+        this.image.src = "./../images/coroface-removebg-preview.png"
+        this.move = 10;
+        // caída
+        this.vy = 0;
+        this.jumpStrength = 8;
+        this.jumps = 0; 
+        this.jumping = false;
+    }
+    
+    draw() {
+        this.vy += GRAVITY;
+        this.y += this.vy;
+        // para que no se salga del margen inferior
+        if (this.y > $canvas.height - this.height)
+            this.y = $canvas.height - this.height;
+            this.jumps = 0; 
+            this.jumping = false;
+        // para que no se salga del margen lateral derecho
+        if (this.x > 900)
+            this.x = 840;
+        // para que no se salga del margen lateral izquierdo
+        if (this.x < 0)
+            this.x = 0;
+        // para que no se salga del margen superior
+        if (this.y < 0 )
+            this.y = 0;
 
-const boardImage = "/images/Background.jpg";
-const board = new Board(0, 0, $canvas.width, $canvas.height, boardImage);
-const coro = new Character (30, $canvas.height / 3, 60, 60);
-const coroImage = "/images/coroface.png";
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+
+    jump() {
+        if (this.jumps >=1) {
+            this.jumping = true;
+        }
+        if (!this.jumping) {
+            this.jumps++;
+            this.vy = -this.jumpStrength;
+        }
+    }
+
+    // métodos de movimientos.
+
+    moveDown() {
+        this.y += this.move;
+    }
+    moveLeft() {
+        this.x -= this.move;
+    }
+    moveRight() {
+        this.x += this.move;
+    }
+    moveUp() {
+        this.y -= this.jump; 
+    }
+    stop() {
+        this.vx = 0;
+    }
+
+    // método para el toque con los obstáculos.
+
+    isTouching(enemy) {
+        return (
+            this.x+20 < enemy.x + enemy.width-7 &&
+			this.x + this.width-20 > enemy.x-7 &&
+			this.y-20 < enemy.y + enemy.height-7 &&
+			this.y + this.height-20 > enemy.y-7
+        );
+    }
+    isTouching(friend) {
+        return (
+            this.x+20 < friend.x + friend.width-7 &&
+			this.x + this.width-20 > friend.x-7 &&
+			this.y-20 < friend.y + friend.height-7 &&
+			this.y + this.height-20 > friend.y-7
+        );
+    }
+    isTouching(vaccine) {
+        return (
+            this.x < vaccine.x + vaccine.width &&
+			this.x + this.width > vaccine.x &&
+			this.y < vaccine.y + vaccine.height &&
+			this.y + this.height > vaccine.y
+        );
+    }
+}
+
+class Enemy extends Character {
+    constructor(x, y) {
+        super(x, y);
+        this.image = new Image ();
+        this.image.src = "./../images/enemyface-removebg-preview.png"
+        this.height = 60
+        this.width = 60
+    }
+    
+    draw() {
+        this.x--;
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+}
+
+class Friend extends Character {
+    constructor(x, y) {
+        super(x, y);
+        this.image = new Image ();
+        this.image.src = "./../images/happyface.png"
+        this.height = 60
+        this.width = 60
+    }
+    
+    draw() {
+        this.x--; 
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);       
+    }
+}
+
+class Vaccine extends Character {
+    constructor(x, y) {
+        super(x, y);
+        this.image = new Image ();
+        this.image.src = "./../images/vaccine.png"
+        this.height = 80
+        this.width = 80
+    }
+    
+    draw() {
+        this.x--; 
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);       
+    }
+}
+/*
+class Bullet {
+    constructor(x, y) {
+
+    }
+}*/
+
+class Score {
+    constructor() {
+        this.x = $canvas.width / 2
+        this.y = 700
+        this.score = 0
+    };
+
+    draw () {
+        ctx.font = "40px sans-serif";
+        ctx.fillText (this.score, $canvas.width / 2, 700)
+        ctx.fillText = "darkviolet"
+    }
+
+    scoreIncrement() {
+        this.score++
+    }
+}
+
+// Instancias de las clases
+
+const board = new Board();
+const coro = new Character (30, 0, 60);
 const allEnemies = [];
 const allFriends = [];
+const allVaccines = [];
 const keys = {};
-const bullets = [];
+let score = new Score
+/*const bullets = [];
 let isGameOver = false;
-let score = new Score;
+;*/
 
-// Funciones principales.
+// Funciones del flujo del juego.
 
-function start() {
+function startGame() {
     if (intervalId) return;
     intervalId = setInterval(() => {
         update();
     }, 1000 / 60);
 }
-
-function update() {
-    frames++;
-    generateEnemies();
-    generateFriends();
-    generateVaccine();
-    checkCollitions();
-    clearCanvas();
-    board.draw();
-    coro.draw();
-    drawEnemy();
-    gameOver();
-    checkKeys();
-}
-
+/*
 function gameOver() {
     if (isGameOver) {
         ctx.font = "40px sans-serif";
         ctx.fillText("Game Over", $canvas.width / 3, $canvas.height / 2);
     }
 }
-
+*/
 // Funciones de apoyo
 
 function clearCanvas() {
@@ -100,9 +238,10 @@ function clearCanvas() {
 
 function generateEnemies() {
     if (frames % 200 === 0) {
-        const x = Math.floor(Math.random() * 380);
-        const enemy = new Enemy (y, 910);
+        const y = Math.floor(Math.random() * 380);
+        const enemy = new Enemy (900, y);
             allEnemies.push(enemy);
+            // para limpiar el array de los enemigos.
             allEnemies.forEach((enemy, index) => {
                 if (enemy.x + enemy.width < 0) allEnemies.splice(1, index);
             });
@@ -115,6 +254,42 @@ function drawEnemy() {
     })
 }
 
+function generateFriends() {
+    if (frames % 300 === 0) {
+        const y = Math.floor(Math.random() * 380);
+        const friend = new Friend (900, y);
+            allFriends.push(friend);
+            // para limpiar el array de los enemigos.
+            allFriends.forEach((friend, index) => {
+                if (friend.x + friend.width < 0) allFriends.splice(1, index);
+            });
+    }
+}
+
+function drawFriend() {
+    allFriends.forEach((friend) => {
+        friend.draw()
+    })
+}
+
+function generateVaccine() {
+    if (frames % 350 === 0) {
+        const y = Math.floor(Math.random() * 380);
+        const vaccine = new Vaccine (900, y);
+            allVaccines.push(vaccine);
+            // para limpiar el array de los enemigos.
+            allVaccines.forEach((vaccine, index) => {
+                if (vaccine.x + vaccine.width < 0) allVaccines.splice(1, index);
+            });
+    }
+}
+
+function drawVaccine() {
+    allVaccines.forEach((vaccine) => {
+        vaccine.draw()
+    })
+}
+
 function checkCollitions() {
     allEnemies.forEach((enemy) => {
         if (coro.isTouching(enemy)) {
@@ -122,8 +297,20 @@ function checkCollitions() {
             isGameOver = true;
         }
     });
+    allFriends.forEach((friend) => {
+        if (coro.isTouching(friend)) {
+            clearInterval(intervalId);
+            isGameOver = true;
+        }
+    });
+    allVaccines.forEach((vaccine) => {
+        if (coro.isTouching(vaccine)) {
+            clearInterval(intervalId);
+            isGameOver = true;
+        }
+    });
 }
-
+/*
 function drawScore() {
     allEnemies.forEach((enemy) => {
         if(enemy.y + enemy.height > coro.y + coro.height) {
@@ -132,29 +319,46 @@ function drawScore() {
     })
     score.draw()
 }
-
+*/
 // Funciones de interacción con el usuario.
 
 function checkKeys() {
-    document.onkeydown = (event) => {
-        switch (event.key) {
-            case "ArrowLeft":
-                coro.moveLeft();
-            break;
-            case "ArrowRigth":
-                coro.moveRight();
-            break;
-            case "ArrowUp":
-                coro.moveUp();
-            break;
-            case "ArrowDown":
-                coro.moveDown();
-            break;
-        
-            default:
-                break;
-        }
-    };
+    if (keys.ArrowLeft) coro.moveLeft();
+    if (keys.ArrowRigth) coro.moveRight();
+    if (keys.ArrowUp) coro.jump();
+    if (keys.ArrowDown) coro.moveDown();
+    /* if (keys.z) {
+       const bullet = new Bullet (coro.x + 15, coro.y);
+       bullet.shootSound();
+       bullets.push*/
+}
+
+document.onkeydown = (event) => {
+    keys[event.key] = true;
+}
+
+document.onkeyup = (event) => {
+    keys[event.key] = false;
+    coro.stop();
+}
+
+// Inicializa el juego.
+
+function update() {
+    frames++;
+    checkKeys();
+    generateEnemies();
+    generateFriends();
+    generateVaccine();
+    clearCanvas();
+    board.draw();
+    coro.draw();
+    drawEnemy();
+    drawFriend();
+    drawVaccine();
+    checkCollitions();
+    //gameOver();
+    //drawScore();
 }
 
 $button.onclick = startGame;
